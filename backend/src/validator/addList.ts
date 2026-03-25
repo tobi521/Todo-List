a5f23192005e4283e437bd1b9d2633167d53accc
tobi521 <tobi04521@outlook.com>
2026-03-25 15:52:23 -0500

    fix:list service

--- backend/src/validator/addList.ts
+++ backend/src/validator/addList.ts
@@ -1,25 +1,18 @@
 import validator from "validator";
 import isEmpty from "./isEmpty";
 
-<<<<<<< HEAD
 type ErrorProps = {
   title?: string;
   description?: string;
 }
 
-=======
->>>>>>> 822c5144d0344f2a5830e37a2a2917e7418a8e73
 export default (
   value: { 
     title: string; 
     description: string 
   }
 ) => {
-<<<<<<< HEAD
   let errors: ErrorProps ={};
-=======
-  let errors: { title?: string, description?: string } ={};
->>>>>>> 822c5144d0344f2a5830e37a2a2917e7418a8e73
 
   value.title = !isEmpty(value.title) ? value.title : "";
   value.description = !isEmpty(value.description) ? value.description : "";
